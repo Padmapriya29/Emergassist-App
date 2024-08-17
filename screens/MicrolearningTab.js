9e0988bd255acdc1774032f11e9fc278d0200ba3
Padmapriya29 <81081235+Padmapriya29@users.noreply.github.com>
2024-08-17 09:02:22 +0530

    Update MicrolearningTab.js

--- screens/MicrolearningTab.js
+++ screens/MicrolearningTab.js
@@ -12,8 +12,8 @@ export default class ReceiverDetailsScreen extends React.Component {
       userName: "",
       receiverId: this.props.navigation.getParam("details")["user_id"],
       requestId: this.props.navigation.getParam("details")["request_id"],
-      bookName: this.props.navigation.getParam("details")["book_name"],
-      bookImage: "#",
+      CourseName: this.props.navigation.getParam("details")["Course_name"],
+      CourseImage: "#",
       reasonForRequesting: this.props.navigation.getParam("details")[
         "reason_to_request"
       ],
@@ -51,14 +51,14 @@ export default class ReceiverDetailsScreen extends React.Component {
         });
       });
 
-    db.collection("requested_books")
+    db.collection("requested_Courses")
       .where("request_id", "==", this.state.requestId)
       .get()
       .then((snapshot) => {
         snapshot.forEach((doc) => {
           this.setState({
             receiverRequestDocId: doc.id,
-            bookImage: doc.data().image_link,
+            CourseImage: doc.data().image_link,
           });
         });
       });
@@ -69,9 +69,9 @@ export default class ReceiverDetailsScreen extends React.Component {
     this.getUserDetails(this.state.userId);
   }
 
-  updateBookStatus = () => {
+  updateCourseStatus = () => {
     db.collection("all_donations").add({
-      book_name: this.state.bookName,
+      Course_name: this.state.CourseName,
       request_id: this.state.requestId,
       requested_by: this.state.receiverName,
       donor_id: this.state.userId,
@@ -81,12 +81,12 @@ export default class ReceiverDetailsScreen extends React.Component {
 
   addNotification = () => {
     var message =
-      this.state.userName + " has shown interest in donating the book";
+      this.state.userName + " has shown interest in donating the Course";
     db.collection("all_notifications").add({
       targeted_user_id: this.state.receiverId,
       donor_id: this.state.userId,
       request_id: this.state.requestId,
-      book_name: this.state.bookName,
+      Course_name: this.state.CourseName,
       date: firebase.firestore.FieldValue.serverTimestamp(),
       notification_status: "unread",
       message: message,
@@ -107,7 +107,7 @@ export default class ReceiverDetailsScreen extends React.Component {
               />
             }
             centerComponent={{
-              text: "Donate Books",
+              text: "Donate Courses",
               style: {
                 color: "#ffffff",
                 fontSize: RFValue(20),
@@ -128,7 +128,7 @@ export default class ReceiverDetailsScreen extends React.Component {
           >
             <View style={{ flex: 0.4 }}>
               <Image
-                source={{ uri: this.state.bookImage }}
+                source={{ uri: this.state.CourseImage }}
                 style={{
                   width: "100%",
                   height: "100%",
@@ -150,7 +150,7 @@ export default class ReceiverDetailsScreen extends React.Component {
                   textAlign: "center",
                 }}
               >
-                {this.state.bookName}
+                {this.state.CourseName}
               </Text>
               <Text
                 style={{
@@ -228,7 +228,7 @@ export default class ReceiverDetailsScreen extends React.Component {
                 <TouchableOpacity
                   style={styles.button}
                   onPress={() => {
-                    this.updateBookStatus();
+                    this.updateCourseStatus();
                     this.addNotification();
                     this.props.navigation.navigate("MyDonations");
                   }}
